b320ff77d50e0cf2cf89bb20ecace691c1be8466
Timgo-1 <timgodesigns@gmail.com>
2022-10-03 01:25:21 +0300

    Fiest Commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -33,7 +33,7 @@ const Sidebar = () => {
   return (
     <>
       <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-sidebar">
-        <img src={logo} alt="logo" className="w-full h-35 object-contain" />
+        <img src={logo} alt="logo" className="w-full h-15 object-contain" />
         <NavLinks />
       </div>
 
